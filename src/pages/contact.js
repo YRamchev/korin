997f7b459529c8cf92423892c8e5effe0686f6ff
yramchev <yramchev@digitalriver.com>
2020-10-12 20:07:45 +0300

    wip

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,33 @@
-import React from "react"
+import React, { useEfect, useMemo } from "react"
 import Layout from "../components/Layout"
 import { Helmet } from "react-helmet"
 
 export default () => {
+  const [name, setName] = useEfect("")
+  const [email, setEmail] = useEfect("")
+  const [message, setMessage] = useEfect("")
+
+  const contact = useMemo(() => {
+    return {
+      name: name,
+      email: email,
+      message: message,
+    }
+  }, [name, email, message])
+
+  const handleSubmit = e => {
+    console.log(contact)
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", contact }),
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error))
+
+    e.preventDefault()
+  }
+
   return (
     <>
       <Helmet title="Contact - Yordan Ramchev" defer={false} />
@@ -17,27 +42,52 @@ export default () => {
             elementum. Proin sed eros nec est maximus iaculis. Proin pharetra
             justo eu convallis gravida. Mauris vestibulum lacinia mi eu blandit.
           </p>
-          <div className="field">
-            <label>
-              <div className="field__label">Your name</div>
-              <input type="text" placeholder="Your name" />
-            </label>
-          </div>
-          <div className="field">
-            <label>
-              <div className="field__label">Your E-mail</div>
-              <input type="text" placeholder="Your E-mail" />
-            </label>
-          </div>
-          <div className="field">
-            <label>
-              <div className="field__label">Message</div>
-              <textarea>Message...</textarea>
-            </label>
-          </div>
-          <button className="btn btn--primary mt-24">Submit</button>
+          <form onSubmit={handleSubmit}>
+            <div className="field">
+              <label>
+                <div className="field__label">Your name</div>
+                <input
+                  type="text"
+                  placeholder="Your name"
+                  value={email}
+                  onChange={e => setName(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="field">
+              <label>
+                <div className="field__label">Your E-mail</div>
+                <input
+                  type="text"
+                  placeholder="Your E-mail"
+                  value={name}
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="field">
+              <label>
+                <div className="field__label">Message</div>
+                <textarea
+                  value={message}
+                  onChange={e => setMessage(e.target.value)}
+                >
+                  Message...
+                </textarea>
+              </label>
+            </div>
+            <button type="submit" className="btn btn--primary mt-24">
+              Submit
+            </button>
+          </form>
         </section>
       </Layout>
     </>
   )
+}
+
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
 }
